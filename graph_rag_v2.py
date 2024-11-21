7559fb53d83d4fc78a4a81c6c30b514c60c8f7c5
papgeokon1 <georgepapakostidis@gmail.com>
2024-11-21 10:14:25 +0200

    Add files via upload

--- graph_rag_v2.py
+++ graph_rag_v2.py
@@ -259,10 +259,10 @@ class KnowledgeGraph:
 
     def _load_spacy_model(self):
         try:
-            return spacy.load("en_core_web_trf")
+            return spacy.load("en_core_web_sm")
         except OSError:
-            download("en_core_web_trf")
-            return spacy.load("en_core_web_trf")
+            download("en_core_web_sm")
+            return spacy.load("en_core_web_sm")
 
     def _extract_concepts_and_entities(self, content, llm):
         if content in self.concept_cache:
